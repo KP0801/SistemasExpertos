46c55e0737766b9bd4ac81bb6e6038956ba8481e
KP0801 <elhondureno05@gmail.com>
2024-06-19 23:13:54 -0600

    Primero

---
+++ tailwind.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+    screen: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        primary: "#fd3d57",
+      },
+    },
+  },
+  plugins: [],
+};
+
